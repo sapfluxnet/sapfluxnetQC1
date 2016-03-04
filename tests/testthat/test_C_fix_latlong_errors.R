0a70709cc0c9cc3bd9fe8746596cc4b789c2f238
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-03-04 10:35:23 +0100

    fix #1 coordinates in countries with positive and negative borders

--- tests/testthat/test_C_fix_latlong_errors.R
+++ tests/testthat/test_C_fix_latlong_errors.R
@@ -3,11 +3,11 @@ library(sapfluxnetr)
 context('C1. Fix latlong errors arguments')
 
 foo_data <- data.frame(
-  longitude = rnorm(5),
-  latitude = rnorm(5),
+  longitude = c(84.4800, 8.6100, 174.4924, 7.0000, 48.0000),
+  latitude = c(10.1800, 39.9000, -36.7959, 46.0000, 23.0000),
   country = c('CRI', 'ITA', 'NZL', 'CHE', 'BRA'),
   site_name = c('bla', 'ble', 'bli', 'blo', 'blu'),
-  is_inside_country = c(FALSE, TRUE, FALSE, FALSE, TRUE)
+  is_inside_country = c(FALSE, TRUE, TRUE, FALSE, FALSE)
 )
 
 test_that('argument errors are correct', {
@@ -23,8 +23,18 @@ test_that('argument errors are correct', {
   expect_error(fix_latlong_errors(foo_data[,c(1:4)]), 'There is no is_inside_country variable')
 })
 
+context('C2. Fix latlong errors result')
+
 test_that('results are a dataframe', {
-  # expect_true(is.data.frame())
+  expect_true(is.data.frame(fix_latlong_errors(foo_data)))
+})
+
+test_that('results are correct (without special countries)', {
+  expect_message(fix_latlong_errors(foo_data), '0 latitude sign errors fixed.\n2 longitude sign errors fixed.\n1 unable to fix due to country borders sharing positive and negative coordinates.\n\n')
+})
+
+test_that('results are correct (with special countries', {
+  expect_message(fix_latlong_errors(foo_data), '0 latitude sign errors fixed.\n2 longitude sign errors fixed.\n1 unable to fix due to country borders sharing positive and negative coordinates.\n\n')
 })
 
 file.remove('CRI_adm0.rds', 'ITA_adm0.rds', 'NZL_adm0.rds', 'CHE_adm0.rds',
