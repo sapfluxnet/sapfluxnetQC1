d4e634e52b2318fab6f1be16898648073cd57480
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-04-01 12:57:40 +0200

    complete rewrite of qc_md_general_checks. Dictionaries are now lists and checks for long metadata is fixed.

--- R/qc_md_general_checks.R
+++ R/qc_md_general_checks.R
@@ -12,16 +12,15 @@
 #'
 #' @family Quality Checks Functions
 #'
-#' @param metadata Name of the metadata sheet of which dictionary is needed.
+#' @param dic_name Name of the metadata sheet of which dictionary is needed.
 #'   It must be one of \code{site_md}, \code{stand_md}, \code{species_md},
 #'   \code{plant_md} or \code{environmental_md}.
 #'
-#' @return A data frame containing two columns, \code{Name} and \code{Class} for
-#'   the selected kind of metadata.
+#' @return A named list, variable names being the index and class the value
 
 # START
 # Function declaration
-create_dic <- function(metadata) {
+create_dic <- function(dic_name) {
 
   # STEP 0
   # Argument checking
@@ -31,89 +30,133 @@ create_dic <- function(metadata) {
   # Get the kind of metadata and populate the dictionary
 
   # 1.1 site metadata
-  if (metadata == 'site_md') {
-    dic <- data.frame(
-      Name = c('si_name', 'si_country', 'si_contact_firstname',
-               'si_contact_lastname', 'si_contact_email', 'si_contact_institution',
-               'si_addcontr_firstname', 'si_addcontr_lastname',
-               'si_addcontr_email', 'si_addcontr_institution', 'si_lat',
-               'si_long', 'si_elev', 'si_igbp', 'si_paper', 'si_dist_mgmt',
-               'si_flux_network', 'si_dendro_network', 'si_remarks', 'si_code'),
-      Class = c('character', 'character', 'character',
-                'character', 'character', 'character',
-                'character', 'character',
-                'character', 'character', 'numeric',
-                'numeric', 'numeric', 'character', 'character', 'character',
-                'logical', 'logical', 'character', 'character')
-    )
+  if (dic_name == 'site_md') {
+    dic <- list(si_name = 'character', si_country = 'character',
+                si_contact_firstname = 'character', si_contact_lastname = 'character',
+                si_contact_email = 'character', si_contact_institution = 'character',
+                si_addcontr_firstname = 'character', si_addcontr_lastname = 'character',
+                si_addcontr_email = 'character', si_addcontr_institution = 'character',
+                si_lat = c('numeric', 'integer'), si_long = c('numeric', 'integer'),
+                si_elev = c('numeric', 'integer'), si_igbp = 'character',
+                si_paper = 'character', si_dist_mgmt = 'character',
+                si_flux_network = 'logical', si_dendro_network = 'logical',
+                si_remarks = 'character', si_code = 'character')
+
+    # dic <- data.frame(
+    #   Name = c('si_name', 'si_country', 'si_contact_firstname',
+    #            'si_contact_lastname', 'si_contact_email', 'si_contact_institution',
+    #            'si_addcontr_firstname', 'si_addcontr_lastname',
+    #            'si_addcontr_email', 'si_addcontr_institution', 'si_lat',
+    #            'si_long', 'si_elev', 'si_igbp', 'si_paper', 'si_dist_mgmt',
+    #            'si_flux_network', 'si_dendro_network', 'si_remarks', 'si_code'),
+    #   Class = c('character', 'character', 'character',
+    #             'character', 'character', 'character',
+    #             'character', 'character',
+    #             'character', 'character', 'numeric',
+    #             'numeric', 'numeric', 'character', 'character', 'character',
+    #             'logical', 'logical', 'character', 'character')
+    # )
 
     # 1.1.1 return dic
     return(dic)
   }
 
   # 1.2 stand metadata
-  if (metadata == 'stand_md'){
-    dic <- data.frame(
-      Name = c('st_age', 'st_aspect', 'st_basal_area', 'st_clay_perc',
-               'st_code', 'st_density', 'st_growth_condition', 'st_height',
-               'st_lai', 'st_remarks', 'st_sand_perc', 'st_silt_perc',
-               'st_soil_depth', 'st_soil_texture', 'st_terrain', 'st_treatment'),
-      Class = c('numeric', 'character', 'numeric', 'numeric',
-                'character', 'character', 'character', 'numeric',
-                'numeric', 'character', 'numeric', 'numeric',
-                'numeric', 'character', 'character', 'character')
-    )
+  if (dic_name == 'stand_md'){
+    dic <- list(st_code = 'character', st_growth_condition = 'character',
+                st_treatment = 'character', st_age = c('numeric', 'integer'),
+                st_height = c('numeric', 'integer'), st_density = c('numeric', 'integer'),
+                st_basal_area = c('numeric', 'integer'), st_lai = c('numeric', 'integer'),
+                st_aspect = 'character', st_terrain = 'character',
+                st_soil_depth = c('numeric', 'integer'), st_soil_texture = 'character',
+                st_sand_perc = c('numeric', 'integer'), st_silt_perc = c('numeric', 'integer'),
+                st_clay_perc = c('numeric', 'integer'), st_remarks = 'character')
+
+    # dic <- data.frame(
+    #   Name = c('st_age', 'st_aspect', 'st_basal_area', 'st_clay_perc',
+    #            'st_code', 'st_density', 'st_growth_condition', 'st_height',
+    #            'st_lai', 'st_remarks', 'st_sand_perc', 'st_silt_perc',
+    #            'st_soil_depth', 'st_soil_texture', 'st_terrain', 'st_treatment'),
+    #   Class = c('numeric', 'character', 'numeric', 'numeric',
+    #             'character', 'character', 'character', 'numeric',
+    #             'numeric', 'character', 'numeric', 'numeric',
+    #             'numeric', 'character', 'character', 'character')
+    # )
 
     # 1.2.1 return dic
     return(dic)
   }
 
   # 1.3 Species metadata
-  if (metadata == 'species_md') {
-    dic <- data.frame(
-      Name = c('sp_basal_area_perc', 'sp_leaf_habit', 'sp_name', 'sp_ntrees'),
-      Class = c('numeric', 'character', 'character', 'numeric')
-    )
+  if (dic_name == 'species_md') {
+    dic <- list(sp_name = 'character', sp_ntrees = c('numeric', 'integer'),
+                sp_leaf_habit = 'character', sp_basal_area_perc = c('numeric', 'integer'))
+
+    # dic <- data.frame(
+    #   Name = c('sp_basal_area_perc', 'sp_leaf_habit', 'sp_name', 'sp_ntrees'),
+    #   Class = c('numeric', 'character', 'character', 'numeric')
+    # )
 
     # 1.3.1 return dic
     return(dic)
   }
 
   # 1.4 Plant metadata
-  if (metadata == 'species_md') {
-    dic <- data.frame(
-      Name = c('pl_age', 'pl_azimut_int', 'pl_bark_thick', 'pl_code',
-               'pl_dbh', 'pl_height', 'pl_leaf_area', 'pl_radial_int',
-               'pl_remarks', 'pl_sap_units', 'pl_sapw_area', 'pl_sapw_depth',
-               'pl_sens_cor_grad', 'pl_sens_cor_zero', 'pl_sens_hgt',
-               'pl_sens_length', 'pl_sens_man', 'pl_sens_meth',
-               'pl_sens_timestep', 'pl_social', 'pl_species', 'pl_treatment'),
-      Class = c('numeric', 'character', 'numeric', 'character',
-                'numeric', 'numeric', 'numeric', 'character',
-                'character', 'character', 'numeric', 'numeric',
-                'character', 'character', 'numeric',
-                'numeric', 'character', 'character',
-                'numeric', 'character', 'character', 'character')
-    )
+  if (dic_name == 'plant_md') {
+    dic <- list(pl_code = 'character', pl_species = 'character',
+                pl_treatment = 'character', pl_dbh = c('numeric', 'integer'),
+                pl_height = c('numeric', 'integer'), pl_age = c('numeric', 'integer'),
+                pl_social = 'character', pl_sapw_area = c('numeric', 'integer'),
+                pl_sapw_depth = c('numeric', 'integer'), pl_bark_thick = c('numeric', 'integer'),
+                pl_leaf_area = c('numeric', 'integer'), pl_sens_meth = 'character',
+                pl_sens_man = 'character', pl_sens_cor_grad = 'character',
+                pl_sens_cor_zero = 'character', pl_sap_units = 'character',
+                pl_sens_length = c('numeric', 'integer'), pl_sens_hgt = c('numeric', 'integer'),
+                pl_sens_timestep = c('numeric', 'integer'), pl_radial_int = 'character',
+                pl_azimut_int = 'character', pl_remarks = 'character')
+
+    # dic <- data.frame(
+    #   Name = c('pl_age', 'pl_azimut_int', 'pl_bark_thick', 'pl_code',
+    #            'pl_dbh', 'pl_height', 'pl_leaf_area', 'pl_radial_int',
+    #            'pl_remarks', 'pl_sap_units', 'pl_sapw_area', 'pl_sapw_depth',
+    #            'pl_sens_cor_grad', 'pl_sens_cor_zero', 'pl_sens_hgt',
+    #            'pl_sens_length', 'pl_sens_man', 'pl_sens_meth',
+    #            'pl_sens_timestep', 'pl_social', 'pl_species', 'pl_treatment'),
+    #   Class = c('numeric', 'character', 'numeric', 'character',
+    #             'numeric', 'numeric', 'numeric', 'character',
+    #             'character', 'character', 'numeric', 'numeric',
+    #             'character', 'character', 'numeric',
+    #             'numeric', 'character', 'character',
+    #             'numeric', 'character', 'character', 'character')
+    # )
 
     # 1.4.1 return dic
     return(dic)
   }
 
   # 1.5 Environmental metadata
-  if (metadata == 'environmental_md') {
-    dic <- data.frame(
-      Name = c('env_leafarea_seasonal', 'env_netrad', 'env_plant_watpot',
-               'env_remarks', 'env_ppdf_in', 'env_precip',
-               'env_rh', 'env_swc_deep_depth', 'env_swc_shallow_depth',
-               'env_sw_in', 'env_ta', 'env_time_daylight',
-               'env_timestep', 'env_time_zone', 'env_vpd', 'env_ws'),
-      Class = c('character', 'character', 'character',
-                'character', 'character', 'character',
-                'character', 'numeric', 'numeric',
-                'character', 'character', 'logical',
-                'numeric', 'character', 'character', 'character')
-    )
+  if (dic_name == 'environmental_md') {
+    dic <- list(env_time_zone = 'character', env_time_daylight = 'logical',
+                env_timestep = c('numeric', 'integer'), env_ta = 'character',
+                env_rh = 'character', env_vpd = 'character',
+                env_sw_in = 'character', env_ppfd_in = 'character',
+                env_netrad = 'character', env_ws = 'character',
+                env_precip = 'character', env_swc_shallow_depth = c('numeric', 'integer'),
+                env_swc_deep_depth = c('numeric', 'integer'), env_plant_watpot = 'character',
+                env_leafarea_seasonal = 'character', env_remarks = 'character')
+
+    # dic <- data.frame(
+    #   Name = c('env_leafarea_seasonal', 'env_netrad', 'env_plant_watpot',
+    #            'env_remarks', 'env_ppdf_in', 'env_precip',
+    #            'env_rh', 'env_swc_deep_depth', 'env_swc_shallow_depth',
+    #            'env_sw_in', 'env_ta', 'env_time_daylight',
+    #            'env_timestep', 'env_time_zone', 'env_vpd', 'env_ws'),
+    #   Class = c('character', 'character', 'character',
+    #             'character', 'character', 'character',
+    #             'character', 'numeric', 'numeric',
+    #             'character', 'character', 'logical',
+    #             'numeric', 'character', 'character', 'character')
+    # )
 
     # 1.5.1 return dic
     return(dic)
@@ -123,82 +166,79 @@ create_dic <- function(metadata) {
 }
 
 ################################################################################
-#' Site metadata column check
+#' Metadata columns check
 #'
-#' \code{qc_site_col} checks if all the site metadata columns were
-#' created and their classes setted correctly
+#' \code{qc_md_cols} checks if the columns of the provided metadata return the
+#' correct class, as well as indicates any NAs present and the absence of any
+#' mandatory variable.
 #'
-#' First qaulity check before continue to more complex checks. Here we are
-#' simply testing if all the columns are there with the correct classes
-#' (numerical for numerical values, character for text fields, etc.).
+#' After loading the metadata, columns classes and presence/absence of mandatory
+#' variables must be checked in order to continue with the quality check
+#' workflow. This function returns a summary of the metadata columns and their
+#' state.
 #'
 #' @family Quality Checks Functions
 #'
-#' @param data Site metadata as obtained from \code{\link{dl_metadata}}
+#' @param metadata Data frame containing the metadata in which the test will be
+#'   made.
 #'
-#' @return A data frame with variables in one column and results of checks in
-#'   the following columns
+#' @param dic Name of the metadata dictionary to use as character. It must be
+#'   one of the following: \code{'stand_md'}, \code{'site_md'},
+#'   \code{'species_md'}, \code{'plant_md'} or \code{'environmental_md'}
+#'
+#' @return A data frame with variable names in one column and result of the
+#'   checks in teh following columns
 #'
 #' @export
 
 # START
 # Function declaration
 
-qc_site_col <- function(data) {
+qc_md_cols <- function(metadata, dic) {
 
   # STEP 0
-  # Arguments checking
+  # Argument checks
 
 
   # STEP 1
-  # Data dictionary to allow checks and vector with names of data
-  dic <- data.frame(
-    variables = c('si_name', 'si_country', 'si_contact_firstname',
-                  'si_contact_lastname', 'si_contact_email', 'si_contact_institution',
-                  'si_addcontr_firstname', 'si_addcontr_lastname',
-                  'si_addcontr_email', 'si_addcontr_institution', 'si_lat',
-                  'si_long', 'si_elev', 'si_igbp', 'si_paper', 'si_dist_mgmt',
-                  'si_flux_network', 'si_dendro_network', 'si_remarks', 'si_code'),
-    class = c('character', 'character', 'character',
-              'character', 'character', 'character',
-              'character', 'character',
-              'character', 'character', 'numeric',
-              'numeric', 'numeric', 'character', 'character', 'character',
-              'logical', 'logical', 'character', 'character')
-  )
-
-  data_variables <- names(data)
-
-  # 1.1 Initiate result objects
-  presence_res <- vector()
-  classes_res <- vector()
-  na_res <- vector()
+  # Initialise result objects and dictionary
+  dictionary <- create_dic(dic) # dictionary
+  md_variables <- names(metadata) # variable names
+  presence_res <- vector() # results of presence test
+  classes_res <- vector() # results of class test
+  det_class_res <- vector() # detected class
+  na_res <- vector() # results of NA test
 
   # STEP 2
-  # Checks (presence and class)
-  for (name in dic$variables) {
-    # presence
-    p_res <- name %in% data_variables
+  # Checks
+  for (name in names(dictionary)) {
+    # 2.1 Presence test
+    p_res <- name %in% md_variables
     presence_res <- c(presence_res, p_res)
-    # class
-    c_res <- identical(class(data[[name]]),
-                       as.character(dic$class[dic$variable == name]))
+
+    # 2.2 Class test
+    d_res <- class(metadata[[name]])
+    c_res <- any(dictionary[[name]] == d_res)
+    # c_res <- identical(class(metadata[[name]]),
+    #                    as.character(dictionary$Class[dictionary$Name == name]))
     classes_res <- c(classes_res, c_res)
-    # NAs
+    det_class_res <- c(det_class_res, d_res)
+
+    # 2.3 NA test
     if (p_res) {
-      n_res <- is.na(data[[name]])
+      n_res <- all(is.na(metadata[[name]]))
       na_res <- c(na_res, n_res)
     } else {
       n_res <- NA
       na_res <- c(na_res, n_res)
     }
-
   }
 
   # STEP 3
   # Create and return the result object
-  result <- data.frame(Variable = dic$variables,
+  result <- data.frame(Variable = names(dictionary),
                        PresenceOK = presence_res,
+                       DetectedClass = det_class_res,
                        ClassOK = classes_res,
                        IsNA = na_res)
 
