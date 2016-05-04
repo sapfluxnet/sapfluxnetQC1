a4615113f5bc30333a7a0a254c3cfd243bf5ad94
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-05-04 15:35:13 +0200

    test for revamped dl_data function added

--- tests/testthat/test_E_data_load.R
+++ tests/testthat/test_E_data_load.R
@@ -23,26 +23,61 @@ test_that('Error raises in case of bad arguments', {
 
 file.remove('foo.data')
 
-file_name <- 'foo.xlsx'
-sapflow_data <- dl_data(file_name, 'sapflow_hd', long = FALSE)
-sapflow_data_long <- dl_data(file_name, 'sapflow_hd', long = TRUE)
-env_data <- dl_data(file_name, 'environmental_hd', long = FALSE)
-env_data_long <- dl_data(file_name, 'environmental_hd', long = TRUE)
+xlsx_name <- 'foo.xlsx'
+csv_name <- 'foo.csv'
+csv_name_2 <- 'foo_env.csv'
+
+sapflow_data <- dl_data(xlsx_name, 'sapflow_hd', long = FALSE)
+sapflow_data_long <- dl_data(xlsx_name, 'sapflow_hd', long = TRUE)
+env_data <- dl_data(xlsx_name, 'environmental_hd', long = FALSE)
+env_data_long <- dl_data(xlsx_name, 'environmental_hd', long = TRUE)
+
+sapf_csv_data <- dl_data(csv_name, 'sapflow_hd', long = FALSE)
+sapf_csv_data_long <- dl_data(csv_name, 'sapflow_hd', long = TRUE)
+env_csv_data <- dl_data(csv_name_2, 'environmental_hd', long = FALSE)
+env_csv_data_long <- dl_data(csv_name_2, 'environmental_hd', long = TRUE)
 
 test_that('function returns data frames', {
   expect_is(sapflow_data, 'data.frame')
   expect_is(sapflow_data_long, 'data.frame')
   expect_is(env_data, 'data.frame')
   expect_is(env_data_long, 'data.frame')
+  expect_is(sapf_csv_data, 'data.frame')
+  expect_is(sapf_csv_data_long, 'data.frame')
+  expect_is(env_csv_data, 'data.frame')
+  expect_is(env_csv_data_long, 'data.frame')
 })
+
+test_that('number of variables is correct', {
+  expect_equal(length(names(sapflow_data)), 14)
+  expect_equal(length(names(sapflow_data_long)), 3)
+  expect_equal(length(names(env_data)), 7)
+  expect_equal(length(names(env_data_long)), 3)
+  expect_equal(length(names(sapflow_data)), 14)
+  expect_equal(length(names(sapflow_data_long)), 3)
+  expect_equal(length(names(env_data)), 7)
+  expect_equal(length(names(env_data_long)), 3)
+})
+
+test_that('number of rows is as expected', {
+  expect_equal(nrow(sapflow_data), 78816)
+  expect_equal(nrow(sapflow_data_long), 1024608)
+  expect_equal(nrow(env_data), 78816)
+  expect_equal(nrow(env_data_long), 472896)
+  expect_equal(nrow(sapflow_data), 78816)
+  expect_equal(nrow(sapflow_data_long), 1024608)
+  expect_equal(nrow(env_data), 78816)
+  expect_equal(nrow(env_data_long), 472896)
+})
+
 
 context('E2. Metadata load')
 
-site_md <- suppressMessages(dl_metadata(file_name, 'site_md'))
-stand_md <- dl_metadata(file_name, 'stand_md', si_code_loc = site_md)
-species_md <- dl_metadata(file_name, 'species_md', si_code_loc = site_md)
-plant_md <- dl_metadata(file_name, 'plant_md', si_code_loc = site_md)
-env_md <- dl_metadata(file_name, 'environmental_md', si_code_loc = site_md)
+site_md <- suppressMessages(dl_metadata(xlsx_name, 'site_md'))
+stand_md <- dl_metadata(xlsx_name, 'stand_md', si_code_loc = site_md)
+species_md <- dl_metadata(xlsx_name, 'species_md', si_code_loc = site_md)
+plant_md <- dl_metadata(xlsx_name, 'plant_md', si_code_loc = site_md)
+env_md <- dl_metadata(xlsx_name, 'environmental_md', si_code_loc = site_md)
 
 test_that('metadata loaded are data frames', {
   expect_is(site_md, 'data.frame')
@@ -53,9 +88,9 @@ test_that('metadata loaded are data frames', {
 })
 
 test_that('metadata errors produces the adequate messages', {
-  expect_error(dl_metadata(file_name, 'ste_md'),
+  expect_error(dl_metadata(xlsx_name, 'ste_md'),
                'Provided sheet name is not')
-  expect_message(dl_metadata(file_name, 'site_md'),
+  expect_message(dl_metadata(xlsx_name, 'site_md'),
                  'si_code_loc set to NULL')
   expect_error(dl_metadata('inexistent_file', 'stand_md', si_code_loc = site_md),
                'File does not exist,')
