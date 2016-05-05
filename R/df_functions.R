686e4d7c9ef3ad012be887e2711f21a1079dc79b
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-05-05 18:42:13 +0200

    df_received_to_raw started

--- R/df_functions.R
+++ R/df_functions.R
@@ -15,7 +15,7 @@
 #' @family Data Flow
 #'
 #' @param parent_dir Character vector indicating the parent directory where
-#'   the structure must be built. Default to current working directory.
+#'   the structure must be built. Default to current directory.
 #'
 #' @return Nothing, dirs are created if they do not exist, or a warning is raised
 #'   if they already exist.
@@ -24,7 +24,7 @@
 
 # START
 # Function declaration
-df_folder_structure <- function(parent_dir = getwd()) {
+df_folder_structure <- function(parent_dir = '.') {
 
   # STEP 0
   # Argument checkings
@@ -45,4 +45,75 @@ df_folder_structure <- function(parent_dir = getwd()) {
   dir.create(file.path(parent_dir, 'Templates'), showWarnings = TRUE)
 
   # END FUNCTION
+}
+
+################################################################################
+#' Move from received to raw data folders
+#'
+#' Check if data file/s already exists and create the folder structure needed to
+#' store them.
+#'
+#' The first step after receiving a data set is to move it from received folder
+#' to raw folder (\code{Data/site_code/site_code_Raw/}).
+#'
+#' @family Data Flow
+#'
+#' @param remove Logical indicating if files in received folder are dropped after
+#'   correct after the file transfer. Default to FALSE.
+#'
+#' @return
+#'
+#' @export
+
+# START
+# Function declaration
+df_received_to_raw <- function(remove = FALSE) {
+
+  # STEP 1
+  # Obtaining the file names in the received folder.
+  files <- list.files('.', pattern = "(environmental|sapflow)\\.csv$|_metadata\\.xls(x)?$")
+
+  # STEP 2
+  # Extract site code from file names
+  codes <- unique(stringr::str_replace(
+    files, "(environmental|sapflow)\\.csv$|_metadata\\.xls(x)?$", ""
+  ))
+
+  # STEP 2
+  # Check if folder with site code already exists
+  for (code in codes) {
+
+    # 2.1 path and file names
+    path <- file.path('..', 'Data', code)
+    path_raw <- file.path(path, 'Raw')
+    file_names <- c(file.path(path_raw, paste(code, 'metadata.xlsx')),
+                    file.path(path_raw, paste(code, 'environmental.csv')),
+                    file.path(path_raw, paste(code, 'sapflow.csv')))
+
+    # 2.2 check presence
+    if (dir.exists(path)) {
+      warning('Folder ', path, ' already exists! ',
+              'This means that site is already in the system')
+      if (any(file.exists(file_names))) {
+        warning('One or more files already exist. ',
+                'Not copying any file, manual intervention needed.')
+        break
+      } else {
+        # aquí iria la copia de los archivos cuando el directorio existe
+        break
+      }
+    } else {
+
+      # STEP 3
+      # Creating the data folder structure for site
+      dir.create(path)
+      dir.create(path_raw)
+      dir.create(file.path(path, 'Lvl_1'))
+      dir.create(file.path(path, 'Lvl_2'))
+
+      # STEP 4
+      # Copy data to raw folder
+      file.copy()
+    }
+  }
 }
