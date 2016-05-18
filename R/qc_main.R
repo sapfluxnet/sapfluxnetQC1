bb6ed1c498ad70caf5d529d7f998cc6c3a8f7c30
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-05-18 12:04:29 +0200

    Updated qc_md_results_table to include all passed checks

--- R/qc_main.R
+++ R/qc_main.R
@@ -48,6 +48,10 @@ qc_md_results_table <- function(md_cols, factor_values,
       step <- c(step, 'Metadata variables presence')
       status <- c(status, 'ERROR')
       description <- c(description, 'One or more variables are missing from metadata')
+    } else {
+      step <- c(step, 'Metadata variables presence')
+      status <- c(status, 'PASS')
+      description <- c(description, 'All metadata variables are present')
     }
 
     # 2.2 ClassOK
@@ -55,6 +59,10 @@ qc_md_results_table <- function(md_cols, factor_values,
       step <- c(step, 'Metadata variables expected class')
       status <- c(status, 'ERROR')
       description <- c(description, 'One or more variables have the wrong class')
+    } else {
+      step <- c(step, 'Metadata variables expected class')
+      status <- c(status, 'PASS')
+      description <- c(description, 'All metadata variables have the correct class')
     }
 
     # 2.3 NAs
@@ -62,6 +70,10 @@ qc_md_results_table <- function(md_cols, factor_values,
       step <- c(step, 'Metadata variables NA presence')
       status <- c(status, 'INFO')
       description <- c(description, 'Some variables have no value')
+    } else {
+      step <- c(step, 'Metadata variables NA presence')
+      status <- c(status, 'PASS')
+      description <- c(description, 'No NAs in metadata')
     }
 
     # STEP 3
@@ -71,6 +83,10 @@ qc_md_results_table <- function(md_cols, factor_values,
       step <- c(step, 'Metadata factor variable values')
       status <- c(status, 'ERROR')
       description <- c(description, 'One or more metadata factor variables have values not accepted')
+    } else {
+      step <- c(step, 'Metadata factor variable values')
+      status <- c(status, 'PASS')
+      description <- c(description, 'All factor variables have accepted values')
     }
 
     # STEP 4
@@ -78,7 +94,11 @@ qc_md_results_table <- function(md_cols, factor_values,
     if (all(is.na(email_check$Is_correct)) | any(!email_check$Is_correct, na.rm = TRUE)) {
       step <- c(step, 'Email check')
       status <- c(status, 'WARNING')
-      description <- c(description, 'Email is missing or in worng format')
+      description <- c(description, 'Email is missing or in wrong format')
+    } else {
+      step <- c(step, 'Email check')
+      status <- c(status, 'PASS')
+      description <- c(description, 'Correct email format')
     }
 
     # STEP 5
@@ -87,6 +107,10 @@ qc_md_results_table <- function(md_cols, factor_values,
       step <- c(step, 'Site coordinates')
       status <- c(status, 'WARNING')
       description <- c(description, 'Site provided coordinates are incorrect and not fixable')
+    } else {
+      step <- c(step, 'Site coordinates')
+      status <- c(status, 'PASS')
+      description <- c(description, 'Site provided correct or fixable coordinates')
     }
 
     # STEP 6
@@ -94,17 +118,25 @@ qc_md_results_table <- function(md_cols, factor_values,
     # 6.1 species md
     if (isTRUE(tryCatch(sapfluxnetr::qc_species_names(species_md$sp_name),
                         error = function(e) return(TRUE)))) {
-      step <- c(step, 'Species names spelling')
+      step <- c(step, 'Species names spelling (species_md)')
       status <- c(status, 'WARNING')
       description <- c(description, 'Species names in Species metadata are mispelled')
+    } else {
+      step <- c(step, 'Species names spelling (species_md)')
+      status <- c(status, 'PASS')
+      description <- c(description, 'No mispelling in species names')
     }
 
     # 6.2 plant md
     if (isTRUE(tryCatch(sapfluxnetr::qc_species_names(plant_md$pl_species),
                         error = function(e) return(TRUE)))) {
-      step <- c(step, 'Species names spelling')
+      step <- c(step, 'Species names spelling (plant_md)')
       status <- c(status, 'WARNING')
       description <- c(description, 'Species names in Plant metadata are mispelled')
+    } else {
+      step <- c(step, 'Species names spelling (plant_md)')
+      status <- c(status, 'PASS')
+      description <- c(description, 'No mispelling in species names')
     }
 
     # STEP 7
@@ -113,6 +145,10 @@ qc_md_results_table <- function(md_cols, factor_values,
       step <- c(step, 'Species names presence in Plant and Species metadata')
       status <- c(status, 'ERROR')
       description <- c(description, 'Species in Plant metadata not match species in Species metadata')
+    } else {
+      step <- c(step, 'Species names presence in Plant and Species metadata')
+      status <- c(status, 'PASS')
+      description <- c(description, 'Species are the same in Plant and Species metadata')
     }
 
     # STEP 8
@@ -120,7 +156,11 @@ qc_md_results_table <- function(md_cols, factor_values,
     if (any(!env_var_presence$Concordance)) {
       step <- c(step, 'Environmental variables presence')
       status <- c(status, 'ERROR')
-      description <- c(description, 'Metadata and Data environmental variables do not agree')
+      description <- c(description, 'Data and Metadata environmental variables do not agree')
+    } else {
+      step <- c(step, 'Environmental variables presence')
+      status <- c(status, 'PASS')
+      description <- c(description, 'Data and Metadata environmental variables agree')
     }
 
     # STEP 9
@@ -130,11 +170,11 @@ qc_md_results_table <- function(md_cols, factor_values,
     # STEP 10
     # Return the datatable
     res_table <- DT::datatable(res, class = 'display', rownames = FALSE,
-                               caption = 'Table: Metadata Quality Check Summary',
+                               caption = 'Table 1: Metadata Quality Check Summary',
                                options = list(dom = 't')) %>%
       DT::formatStyle('Status',
-                      backgroundColor = DT::styleEqual(c('INFO', 'WARNING', 'ERROR'),
-                                                       c('#89C4F4', '#F5AB35', '#F22613')))
+                      backgroundColor = DT::styleEqual(c('PASS', 'INFO', 'WARNING', 'ERROR'),
+                                                       c('#26a65b', '#89c4f4', '#f39c12', '#d91e18')))
 
     return(res_table)
 
