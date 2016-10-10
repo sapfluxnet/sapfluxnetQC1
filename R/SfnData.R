975a5a1541e9485a155dde9b5e93bc2df1aa505b
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-10-10 16:49:37 +0200

    added SfnData class and genereics. Error in the methods

---
+++ R/SfnData.R
@@ -0,0 +1,175 @@
+#' An S4 class for sapfluxnet data
+#'
+#' @slot sapf A data frame with the sapf data
+#'
+#' @slot env A data frame with the env data
+#'
+#' @slot sapf_flags A data frame with the same dimensions of \code{sapf} with the
+#'   flag info for each tree/TIMESTAMP combination
+#'
+#' @slot env_flags A data frame with the same dimensions of \code{env} with the
+#'   flag info for each tree/TIMESTAMP combination
+#'
+#' @slot sapf_timestamp A posix vector with the TIMESTAMP for \code{sapf}
+#'
+#' @slot env_timestamp A posix vector with the TIMESTAMP for \code{env}
+#'
+#' @slot si_code A character indicating the site code
+#'
+#' @slot site_md A data frame containing the site metadata
+#'
+#' @slot stand_md A data frame containing the stand metadata
+#'
+#' @slot species_md A data frame containing the species metadata
+#'
+#' @slot plant_md A data frame containing the plant metadata
+#'
+#' @slot env_md A data frame containing the env metadata
+
+SfnData <- setClass(
+  'SfnData',
+  slots = list(
+    sapf = "data.frame",
+    env = "data.frame",
+    sapf_flags = "data.frame",
+    env_flags = "data.frame",
+    sapf_timestamp = "POSIXt",
+    env_timestamp = "POSIXt",
+    si_code = "character",
+    site_md = "data.frame",
+    stand_md = "data.frame",
+    species_md = "data.frame",
+    plant_md = "data.frame",
+    env_md = "data.frame"
+  )
+)
+
+#' SfnData custom get generics
+#'
+#' Generics for getting the info in the slots of SfnData
+#'
+#' @name sfn_get_generics
+NULL
+
+#' @rdname sfn_get_generics
+get_sapf <- setGeneric(
+  "get_sapf",
+  function(object, ...) {
+    standardGeneric("get_sapf")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_env <- setGeneric(
+  "get_env",
+  function(object, ...) {
+    standardGeneric("get_env")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_sapf_flags <- setGeneric(
+  "get_sapf_flags",
+  function(object, ...) {
+    standardGeneric("get_sapf_flags")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_env_flags <- setGeneric(
+  "get_env_flags",
+  function(object, ...) {
+    standardGeneric("get_env_flags")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_sapf_timestamp <- setGeneric(
+  "get_sapf_timestamp",
+  function(object, ...) {
+    standardGeneric("get_sapf_timestamp")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_env_timestamp <- setGeneric(
+  "get_env_timestamp",
+  function(object, ...) {
+    standardGeneric("get_env_timestamp")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_site_md <- setGeneric(
+  "get_site_md",
+  function(object, ...) {
+    standardGeneric("get_site_md")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_stand_md <- setGeneric(
+  "get_stand_md",
+  function(object, ...) {
+    standardGeneric("get_stand_md")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_species_md <- setGeneric(
+  "get_species_md",
+  function(object, ...) {
+    standardGeneric("get_species_md")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_plant_md <- setGeneric(
+  "get_plant_md",
+  function(object, ...) {
+    standardGeneric("get_plant_md")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_env_md <- setGeneric(
+  "get_env_md",
+  function(object, ...) {
+    standardGeneric("get_env_md")
+  }
+)
+
+#' @rdname sfn_get_generics
+get_si_code <- setGeneric(
+  "get_si_code",
+  function(object, ...) {
+    standardGeneric("get_si_code")
+  }
+)
+
+#' SfnData get methods
+#'
+#' Methods to get the info from the SfnData class slots
+#'
+#' @param object Object of class SfnData from which data is retrieved
+#'
+#' @param ... Further arguments to pass on to methods
+#'
+#' @name sfn_get_methods
+NULL
+
+#' @rdname sfn_get_methods
+setMethod(
+  "get_sapf", "SfnData",
+  function(object) {
+    object@sapf
+  }
+)
+
+#' @rdname sfn_get_methods
+setMethod(
+  "get_env", "SfnData",
+  function(object) {
+    object@env
+  }
+)
