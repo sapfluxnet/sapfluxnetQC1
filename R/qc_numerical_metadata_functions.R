e3bbc4835690cb760fe5a86ed9b519624c71fdd5
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-03-06 15:34:46 +0100

    tests for qc_range_dic added

--- R/qc_numerical_metadata_functions.R
+++ R/qc_numerical_metadata_functions.R
@@ -1,177 +1,177 @@
-################################################################################
-#' Find if provided values of a numerical variable are in a suitable range
-#'
-#' \code{qc_suitable_range} is used to determine if a variable has strange
-#' values out of natural or biological expected range.
-#'
-#' Checks are made based on the variable and acceptable ranges provided.
-#' \code{variables} and \code{ranges} must be of the same length, as the first
-#' variable is compared with the first range provided, and so on. If both
-#' objects are not of the same length, an informative error appears and function
-#' stops.
-#'
-#' @family Quality Check Functions
-#'
-#' @param data Data frame containing the variables to check
-#'
-#' @param variables Character vector with variables names
-#'
-#' @param ranges List in the form of
-#'   \code{list("variable1" = c(min, max), "variable2" = c(min, max)...)}
-#'   indicating the suitable ranges for every variable contained in
-#'   \code{variable} (See details).
-#'
-#' @return A logical vector indicating if variables are in range
-#'
-#' @export
-
-
-# START
-# Function declaration
-
-qc_suitable_range <- function(data, variables, ranges,
-                              parent_logger = 'test') {
-
-  # Using calling handlers to logging
-  withCallingHandlers({
-
-    # STEP 0
-    # Argument checks
-    # Is ranges a list?
-    if (!is.list(ranges)) {
-      stop('ranges argument provided is not a list, please provide a list in
-           the form of list("variable1" = c(min, max), "variable2" = c(min, max)...)')
-    }
-    # Is data a data.frame?
-    if (!is.data.frame(data)) {
-      stop('provided data is not a data.frame, please review data object provided')
-    }
-    # Is variables a character vector?
-    if (!is.character(variables)) {
-      stop('variables are not provided as character vector, please review
-           variables object')
-    }
-    # Are variables and ranges of the same length?
-    if (length(variables) != length(ranges)) {
-      stop('variables vector and ranges list are not of the same length, please
-           review provided objects')
-    }
-    # Are variables present in data?
-    if (!all(variables %in% data)) {
-      stop('one or more variables are nor present in data, please make sure that
-           provided data and variables are correct')
-  }
-
-  # STEP 1
-  # Extract the variable values
-  var_values <- lapply(variables, function(x) {
-    data[[x]]
-  })
-
-  # STEP 2
-  # Check if values are in range
-
-  # 2.1 Empty results vector
-  res <- vector()
-  # 2.2 For loop to see if each variable value is in range
-  for (i in 1:length(var_values)) {
-    tmp_res <- var_values[i] >= ranges[[i]][1] && var_values[i] <= ranges[[i]][2]
-    res <- c(res, tmp_res)
-  }
-
-  # STEP 3
-  # Return the results
-  return(res)
-  # END FUNCTION
-  },
-
-  # handlers
-  warning = function(w){logging::logwarn(w$message,
-                                         logger = paste(parent_logger,
-                                                        'qc_suitable_range', sep = '.'))},
-  error = function(e){logging::logerror(e$message,
-                                        logger = paste(parent_logger,
-                                                       'qc_suitable_range', sep = '.'))},
-  message = function(m){logging::loginfo(m$message,
-                                         logger = paste(parent_logger,
-                                                        'qc_suitable_range', sep = '.'))})
-
-}
-
-################################################################################
-#' Suitable Range for sapflow values
-#'
-#' Suitability check for sapflow values.
-#'
-#' Ranges for sapflow measures at sapwood level are obtained from literature,
-#' being the most extreme values -10 and 533 cm3cm-2h-1 (Manzoni et al 2013 and
-#' other sources). This range is only informative, but values outside of the
-#' range may indicate some error in the data.
-#'
-#' @family Quality Checks Functions
-#'
-#' @param data Data frame with the values of sapflow measures for each plant
-#'   (variable).
-#'
-#' @return A data frame with summarising the values outside of the suitable
-#'   range.
-#'
-#' @export
-
-# START
-# Function declaration
-qc_sapf_range_check <- function(data, parent_logger = 'test') {
-
-  # Using calling handlers to manage errors
-  withCallingHandlers({
-
-    # STEP 0
-    # Argument checks
-    # is data a data frame?
-    if(!is.data.frame(data)) {
-      stop('data provided is not a data frame')
-    }
-
-    # STEP 1
-    # Initialising res objects
-    names <- names(data[,-1])
-    plant <- vector()
-    value <- vector()
-
-    # STEP 2
-    # For loop to iterate between plants
-    for (name in names) {
-      value <- c(value, data[data[, name] < -10 | data[, name] > 533, name])
-      plant <- c(plant, rep(name,
-                            length(data[data[, name] < -10 | data[, name] > 533, name])))
-    }
-
-    # STEP 3
-    # Build the results data frame
-    res <- data.frame(
-      Plant = plant,
-      Value = value,
-      Max = 533,
-      Min = -10,
-      stringsAsFactors = FALSE
-    )
-
-    # 3.1 Return the results
-    return(res)
-
-  },
-
-  # handlers
-  warning = function(w){logging::logwarn(w$message,
-                                         logger = paste(parent_logger,
-                                                        'qc_sapf_range_check', sep = '.'))},
-  error = function(e){logging::logerror(e$message,
-                                        logger = paste(parent_logger,
-                                                       'qc_sapf_range_check', sep = '.'))},
-  message = function(m){logging::loginfo(m$message,
-                                         logger = paste(parent_logger,
-                                                        'qc_sapf_range_check', sep = '.'))})
-}
+# ################################################################################
+# #' Find if provided values of a numerical variable are in a suitable range
+# #'
+# #' \code{qc_suitable_range} is used to determine if a variable has strange
+# #' values out of natural or biological expected range.
+# #'
+# #' Checks are made based on the variable and acceptable ranges provided.
+# #' \code{variables} and \code{ranges} must be of the same length, as the first
+# #' variable is compared with the first range provided, and so on. If both
+# #' objects are not of the same length, an informative error appears and function
+# #' stops.
+# #'
+# #' @family Quality Check Functions
+# #'
+# #' @param data Data frame containing the variables to check
+# #'
+# #' @param variables Character vector with variables names
+# #'
+# #' @param ranges List in the form of
+# #'   \code{list("variable1" = c(min, max), "variable2" = c(min, max)...)}
+# #'   indicating the suitable ranges for every variable contained in
+# #'   \code{variable} (See details).
+# #'
+# #' @return A logical vector indicating if variables are in range
+# #'
+# #' @export
+#
+#
+# # START
+# # Function declaration
+#
+# qc_suitable_range <- function(data, variables, ranges,
+#                               parent_logger = 'test') {
+#
+#   # Using calling handlers to logging
+#   withCallingHandlers({
+#
+#     # STEP 0
+#     # Argument checks
+#     # Is ranges a list?
+#     if (!is.list(ranges)) {
+#       stop('ranges argument provided is not a list, please provide a list in
+#            the form of list("variable1" = c(min, max), "variable2" = c(min, max)...)')
+#     }
+#     # Is data a data.frame?
+#     if (!is.data.frame(data)) {
+#       stop('provided data is not a data.frame, please review data object provided')
+#     }
+#     # Is variables a character vector?
+#     if (!is.character(variables)) {
+#       stop('variables are not provided as character vector, please review
+#            variables object')
+#     }
+#     # Are variables and ranges of the same length?
+#     if (length(variables) != length(ranges)) {
+#       stop('variables vector and ranges list are not of the same length, please
+#            review provided objects')
+#     }
+#     # Are variables present in data?
+#     if (!all(variables %in% data)) {
+#       stop('one or more variables are nor present in data, please make sure that
+#            provided data and variables are correct')
+#   }
+#
+#   # STEP 1
+#   # Extract the variable values
+#   var_values <- lapply(variables, function(x) {
+#     data[[x]]
+#   })
+#
+#   # STEP 2
+#   # Check if values are in range
+#
+#   # 2.1 Empty results vector
+#   res <- vector()
+#   # 2.2 For loop to see if each variable value is in range
+#   for (i in 1:length(var_values)) {
+#     tmp_res <- var_values[i] >= ranges[[i]][1] && var_values[i] <= ranges[[i]][2]
+#     res <- c(res, tmp_res)
+#   }
+#
+#   # STEP 3
+#   # Return the results
+#   return(res)
+#   # END FUNCTION
+#   },
+#
+#   # handlers
+#   warning = function(w){logging::logwarn(w$message,
+#                                          logger = paste(parent_logger,
+#                                                         'qc_suitable_range', sep = '.'))},
+#   error = function(e){logging::logerror(e$message,
+#                                         logger = paste(parent_logger,
+#                                                        'qc_suitable_range', sep = '.'))},
+#   message = function(m){logging::loginfo(m$message,
+#                                          logger = paste(parent_logger,
+#                                                         'qc_suitable_range', sep = '.'))})
+#
+# }
+#
+# ################################################################################
+# #' Suitable Range for sapflow values
+# #'
+# #' Suitability check for sapflow values.
+# #'
+# #' Ranges for sapflow measures at sapwood level are obtained from literature,
+# #' being the most extreme values -10 and 533 cm3cm-2h-1 (Manzoni et al 2013 and
+# #' other sources). This range is only informative, but values outside of the
+# #' range may indicate some error in the data.
+# #'
+# #' @family Quality Checks Functions
+# #'
+# #' @param data Data frame with the values of sapflow measures for each plant
+# #'   (variable).
+# #'
+# #' @return A data frame with summarising the values outside of the suitable
+# #'   range.
+# #'
+# #' @export
+#
+# # START
+# # Function declaration
+# qc_sapf_range_check <- function(data, parent_logger = 'test') {
+#
+#   # Using calling handlers to manage errors
+#   withCallingHandlers({
+#
+#     # STEP 0
+#     # Argument checks
+#     # is data a data frame?
+#     if(!is.data.frame(data)) {
+#       stop('data provided is not a data frame')
+#     }
+#
+#     # STEP 1
+#     # Initialising res objects
+#     names <- names(data[,-1])
+#     plant <- vector()
+#     value <- vector()
+#
+#     # STEP 2
+#     # For loop to iterate between plants
+#     for (name in names) {
+#       value <- c(value, data[data[, name] < -10 | data[, name] > 533, name])
+#       plant <- c(plant, rep(name,
+#                             length(data[data[, name] < -10 | data[, name] > 533, name])))
+#     }
+#
+#     # STEP 3
+#     # Build the results data frame
+#     res <- data.frame(
+#       Plant = plant,
+#       Value = value,
+#       Max = 533,
+#       Min = -10,
+#       stringsAsFactors = FALSE
+#     )
+#
+#     # 3.1 Return the results
+#     return(res)
+#
+#   },
+#
+#   # handlers
+#   warning = function(w){logging::logwarn(w$message,
+#                                          logger = paste(parent_logger,
+#                                                         'qc_sapf_range_check', sep = '.'))},
+#   error = function(e){logging::logerror(e$message,
+#                                         logger = paste(parent_logger,
+#                                                        'qc_sapf_range_check', sep = '.'))},
+#   message = function(m){logging::loginfo(m$message,
+#                                          logger = paste(parent_logger,
+#                                                         'qc_sapf_range_check', sep = '.'))})
+# }
 
 ################################################################################
 #' Ranges dictionary
@@ -227,7 +227,8 @@ qc_range_dic <- function(parent_logger = 'test') {
       netrad = netrad_range,
       ws = ws_range,
       precip = precip_range,
-      sapf = sapf_range
+      sapf_sapw = sapf_sapw_range,
+      sapf_tree = sapf_tree_range
     )
 
     # STEP 3
--- tests/testthat/test_L_ranges.R
+++ tests/testthat/test_L_ranges.R
@@ -1,35 +1,21 @@
 library(sapfluxnetQC1)
 
-context('L1. Sapflow ranges tests')
+context('L1. Ranges')
 
-set.seed(25)
-
-foo_data <- data.frame(
-  TIMESTAMP = rep('No date', 100),
-  Tree_1 = rnorm(100, 250, 125),
-  Tree_2 = rnorm(100, 65, 5),
-  Tree_3 = rnorm(100, 25, 15),
-  Tree_4 = rnorm(100, 400, 125),
-  Tree_5 = rnorm(100, 200, 50)
-)
-
-set.seed(NULL)
-
-foo_res <- qc_sapf_range_check(foo_data)
-
-test_that('Arguments checks works', {
-  expect_error(qc_sapf_range_check('foo_data'),
-               'data provided is not a data frame')
-})
-
-test_that('Function works as expected', {
-  expect_is(foo_res, 'data.frame')
-  expect_length(foo_res[,1], 20)
-  expect_length(foo_res[foo_res$Plant == 'Tree_1', 'Value'], 2)
-  expect_length(foo_res[foo_res$Plant == 'Tree_2', 'Value'], 0)
-  expect_length(foo_res[foo_res$Plant == 'Tree_3', 'Value'], 2)
-  expect_length(foo_res[foo_res$Plant == 'Tree_4', 'Value'], 16)
-  expect_length(foo_res[foo_res$Plant == 'Tree_5', 'Value'], 0)
-  expect_length(foo_res[foo_res$Value > 533, 'Value'], 17)
-  expect_length(foo_res[foo_res$Value < -10, 'Value'], 3)
+test_that('ranges dictionary is created ok', {
+  expect_is(qc_range_dic(), 'list')
+  expect_length(qc_range_dic(), 10)
+  expect_identical(names(qc_range_dic()),
+                   c('ta', 'rh', 'vpd', 'sw_in', 'ppdf_in', 'netrad',
+                     'ws', 'precip', 'sapf_sapw', 'sapf_tree'))
+  expect_length(qc_range_dic()[[1]], 2)
+  expect_length(qc_range_dic()[[2]], 2)
+  expect_length(qc_range_dic()[[3]], 2)
+  expect_length(qc_range_dic()[[4]], 2)
+  expect_length(qc_range_dic()[[5]], 2)
+  expect_length(qc_range_dic()[[6]], 2)
+  expect_length(qc_range_dic()[[7]], 2)
+  expect_length(qc_range_dic()[[8]], 2)
+  expect_length(qc_range_dic()[[9]], 2)
+  expect_length(qc_range_dic()[[10]], 2)
 })
